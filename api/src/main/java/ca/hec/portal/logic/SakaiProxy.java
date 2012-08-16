f7ee1c413b572da0eef778753dde38b535906af0
Curtis van Osch <curtis.van-osch@hec.ca>
2012-08-16 17:57:33 +0000

    ZCGH-65: implemantation de la regle d'affaire pour le pdf

--- api/src/main/java/ca/hec/portal/logic/SakaiProxy.java
+++ api/src/main/java/ca/hec/portal/logic/SakaiProxy.java
@@ -1,6 +1,5 @@
 package ca.hec.portal.logic;
 
-
 /**
  * An interface to abstract all Sakai related API calls in a central method that can be injected into our app.
  * 
@@ -10,7 +9,8 @@ package ca.hec.portal.logic;
 public interface SakaiProxy {
 	
 	/**
-	 * Gets the specific course title associated to the given catalog description
+	 * Gets the specific course site title associated to the given catalog description. (Same course id, from a 
+	 * session that has been completed, and with a published OpenSyllabus course outline)
 	 *
 	 * @param courseId	the course id of the catalog description
 	 * @return the title of the most recently created course offering (that has been published)
--- impl/src/main/java/ca/hec/portal/logic/SakaiProxyImpl.java
+++ impl/src/main/java/ca/hec/portal/logic/SakaiProxyImpl.java
@@ -1,11 +1,17 @@
 package ca.hec.portal.logic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import lombok.Setter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.coursemanagement.api.AcademicSession;
+import org.sakaiproject.coursemanagement.api.CourseManagementService;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
 
@@ -22,21 +28,71 @@ public class SakaiProxyImpl implements SakaiProxy {
 
     @Setter
     private OsylSiteService osylSiteService;
+    
+    @Setter 
+    private CourseManagementService cmService;
 
+    // Class for comparing the academic sessions by Id Descending.
+    public class ComparableAcademicSession implements Comparator<AcademicSession> {	 
+	    public int compare(AcademicSession o1, AcademicSession o2) {
+		return o2.getEid().compareTo(o1.getEid());
+	    }
+	}
+    
     /**
      * {@inheritDoc}
      */
     public String getSpecificCourse(String courseId) {
-	List<Site> sites = siteService.getSites(SiteService.SelectionType.ANY, "course",
-		courseId, null, SiteService.SortType.CREATED_ON_DESC, null);
+	Date today = new Date();
 	
-	for (Site s : sites) {
-	    if (osylSiteService.hasBeenPublished(s.getId()))
-		return s.getTitle();
+	List<AcademicSession> sessions = cmService.getAcademicSessions();
+	Collections.sort(sessions, new ComparableAcademicSession());
+	
+	ArrayList<String> sessionNames = new ArrayList<String>();
+
+	// get all academic sessions, and add their formatted names to the list
+	for (AcademicSession session : sessions) {
+	    String sessionName = getSessionName(session);
+	    
+	    // if the session is completed and not yet in the list, add it
+	    if (session.getEndDate().before(today) && !sessions.contains(sessionName)) {
+		sessionNames.add(sessionName);
+	    }
 	}
+	
+	// Start searching the sessions for a course site with a published course outline to use
+	for (String sessionName : sessionNames) {
+	    // Get the sites with the given Id and Session in the title
+	    List<Site> sites = siteService.getSites(SiteService.SelectionType.ANY, "course",
+		courseId + "." + sessionName, null, SiteService.SortType.TITLE_ASC, null);
+
+	    for (Site s : sites) {
+		if (osylSiteService.hasBeenPublished(s.getId())) {
+		    return s.getTitle();
+		}
+	    }
+	}
+		
 	return "";
     }
 
+    private String getSessionName(AcademicSession session) {
+	String sessionName = null;
+	String sessionId = session.getEid();
+	Date startDate = session.getStartDate();
+	String year = startDate.toString().substring(0, 4);
+
+	if ((sessionId.charAt(3)) == '1')
+	    sessionName = OsylSiteService.WINTER + year;
+	if ((sessionId.charAt(3)) == '2')
+	    sessionName = OsylSiteService.SUMMER + year;
+	if ((sessionId.charAt(3)) == '3')
+	    sessionName = OsylSiteService.FALL + year;
+
+	return sessionName;
+    }
+
+    
     /**
      * init - perform any actions required here for when this bean starts up
      */

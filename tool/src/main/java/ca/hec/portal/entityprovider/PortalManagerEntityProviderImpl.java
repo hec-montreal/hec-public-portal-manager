57fe2301c279bca590298b9598f6af91dc879a4b
Mame Awa Diop <mame-awa.diop@hec.ca>
2017-10-25 12:45:44 -0400

    ZCII-2770: Connecter le portail public directement avec la vue matérialisé psft - changements portail

--- tool/src/main/java/ca/hec/portal/entityprovider/PortalManagerEntityProviderImpl.java
+++ tool/src/main/java/ca/hec/portal/entityprovider/PortalManagerEntityProviderImpl.java
@@ -1,29 +1,26 @@
 package ca.hec.portal.entityprovider;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import ca.hec.commons.utils.FormatUtils;
+import ca.hec.portal.api.OfficialCourseDescriptionService;
+import ca.hec.portal.api.PortalManagerService;
+import ca.hec.portal.logic.SakaiProxy;
+import ca.hec.portal.model.OfficialCourseDescription;
+import ca.hec.portal.model.SimpleOfficialCourseDescription;
 import lombok.Setter;
-
 import org.apache.commons.lang.StringUtils;
 import org.sakaiproject.entitybroker.EntityReference;
 import org.sakaiproject.entitybroker.EntityView;
 import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.annotations.EntityCustomAction;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.ActionsExecutable;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.Resolvable;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.Sampleable;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.*;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
+import org.sakaiproject.entitybroker.entityprovider.search.Search;
 import org.sakaiproject.entitybroker.exception.EntityNotFoundException;
 import org.sakaiproject.entitybroker.exception.FormatUnsupportedException;
 import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import ca.hec.portal.logic.SakaiProxy;
-import ca.hec.portal.api.PortalManagerService;
+import java.util.*;
 
 public class PortalManagerEntityProviderImpl extends AbstractEntityProvider
 	implements CoreEntityProvider, AutoRegisterEntityProvider, Resolvable, Sampleable, Outputable, ActionsExecutable {
@@ -34,6 +31,10 @@ public class PortalManagerEntityProviderImpl extends AbstractEntityProvider
     @Autowired
     private PortalManagerService portalManagerService;
 
+	@Setter
+	@Autowired
+	private OfficialCourseDescriptionService officialCourseDescriptionService;
+
     @Setter
     @Autowired
     private SakaiProxy sakaiProxy;
@@ -42,7 +43,95 @@ public class PortalManagerEntityProviderImpl extends AbstractEntityProvider
 	return new String[] { Formats.JSON, Formats.HTML, Formats.XML };
     }
 
-    @EntityCustomAction(action = "getDepartments", viewKey = EntityView.VIEW_LIST)
+
+	@EntityCustomAction(action = "getOfficialCourseDescription", viewKey = EntityView.VIEW_LIST)
+	public Object getOfficialCourseDescription(EntityView view, Map<String, Object> params) {
+		String courseId = view.getPathSegment(2);
+		return officialCourseDescriptionService.getOfficialCourseDescription(courseId);
+	}
+
+	@EntityCustomAction(action = "getOfficialCourseDescriptions", viewKey = EntityView.VIEW_LIST)
+	public List<?> getOfficialCourseDescriptions(EntityView view, Map<String, Object> params) {
+		Search search = new Search();
+		List<String> seachScopesList = null;
+		Map<String, String> eqCriteria = new HashMap<String, String>();
+		Map<String, String> seachCriteria = new HashMap<String, String>();
+		String searchWords = null;
+		String searchScope = (String)params.get("searchScope");
+		List<SimpleOfficialCourseDescription> listSimpleOfficialCourseDescriptions;
+
+		if (searchScope != null){
+			seachScopesList = Arrays.asList(searchScope.split(","));
+		}
+
+		for (String  param : params.keySet()) {
+			if (param.equals("department")
+					|| param.equals("career")
+					|| param.equals("courseId")) {
+				eqCriteria.put(param, (String) params.get(param));
+			}
+
+			if (param.equals("searchWords")) {
+				String stringSearch =(String)params.get(param);
+				for (String scope : seachScopesList) {
+					seachCriteria.put(scope, stringSearch);
+				}
+			}
+		}
+
+		listSimpleOfficialCourseDescriptions =
+				simplifyOfficialCourseDescriptions(officialCourseDescriptionService.getOfficialCourseDescriptions(
+						eqCriteria, seachCriteria));
+
+		// unless we have search criteria (in this case we sort by search
+		// criteria), we sort catalog descriptions according to the session #,
+		// course # and year (we get it from courseId parameter)
+		if (seachCriteria.isEmpty()) {
+			Collections.sort(listSimpleOfficialCourseDescriptions);
+		}
+
+		return listSimpleOfficialCourseDescriptions;
+	}
+
+	public List<SimpleOfficialCourseDescription> simplifyOfficialCourseDescriptions(
+			List<OfficialCourseDescription> officialCourseDescriptions) {
+		List<SimpleOfficialCourseDescription> simpleOfficialCourseDescriptions =
+				new ArrayList<SimpleOfficialCourseDescription>();
+
+		// convert raw OfficialCourseDescriptions into decorated catalog descriptions
+		for (OfficialCourseDescription cd : officialCourseDescriptions) {
+
+			SimpleOfficialCourseDescription simpleCd = simplifyOfficialCourseDescription(cd);
+			if (simpleCd.getDepartmentGroup() != null) {
+				simpleOfficialCourseDescriptions.add(simpleCd);
+			}
+		}
+
+		return simpleOfficialCourseDescriptions;
+	}
+
+	public SimpleOfficialCourseDescription simplifyOfficialCourseDescription(
+			OfficialCourseDescription cd) {
+		SimpleOfficialCourseDescription scd = new SimpleOfficialCourseDescription();
+
+		scd.setTitle(cd.getTitle());
+		scd.setDescription(cd.getDescription());
+		scd.setDepartmentGroup(portalManagerService.getDepartmentGroup(cd.getDepartment()));
+		scd.setCareerGroup(portalManagerService.getCareerGroup(cd.getCareer()));
+		scd.setRequirements(cd.getRequirements());
+		scd.setCourseId(cd.getCourseId());
+		scd.setHyphenatedCourseId(FormatUtils.formatCourseId(cd.getCourseId()));
+		scd.setCredits("" + cd.getCredits());
+
+		String lang = cd.getLanguage();
+		if (lang != null)
+			scd.setLang(lang.substring(0, 2));
+
+		return scd;
+	}
+
+
+	@EntityCustomAction(action = "getDepartments", viewKey = EntityView.VIEW_LIST)
     public List<?> getDepartments(EntityView view, Map<String, Object> params) {
 	String locale = view.getPathSegment(2);
 	return portalManagerService.getDepartments(locale);
